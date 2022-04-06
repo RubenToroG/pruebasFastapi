61b49a064ffe86e769257b9f99b3c62e47a15a16
Ruben Toro <rubensdt01@gmail.com>
2022-04-06 16:52:38 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+class User(BaseModel):
+    email: str
+    nick_name: str
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None 
+    profile_picture: Optional[str] = None #null
 
 @app.get('/')
 def home():
     return {'Hello': 'World'}
+
+
+    #Request and Response Body
+@app.post('/user/new')
+def create_user(user: User = Body(...)):
+    return user
+
+# ... dentro de fastapi, los 3 puntos indican que el parametro es obligatorio
